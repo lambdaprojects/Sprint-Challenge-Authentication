586a85539249df9c6b7472268e68775117fad813
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-14 10:34:01 -0500

    SprintChallenge - Authentication - MVP Complete
    The login is complete and a token is generated on successful login. Tried to access the jokes using the token generated and it works.

---
+++ auth/secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+  jwtSecret:
+    process.env.JWT_SECRET ||
+    "add a .env file to root of project with the JWT_SECRET variable"
+};
--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const UserHelper = require("../user/user-model.js");
+const jwtKey = require("../auth/secrets.js");
 
 const { authenticate } = require("../auth/authenticate");
 
@@ -25,8 +26,20 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  try {
+    let { username, password } = req.body;
+    let user = await UserHelper.findBy({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ message: `Welcome! ${user.username}`, token });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
 
 function getJokes(req, res) {
@@ -42,4 +55,17 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, jwtKey.jwtSecret, options);
 }
