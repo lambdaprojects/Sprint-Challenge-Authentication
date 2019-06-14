2c2634f32b40b192cd71996cac9ab95e294616b1
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-14 12:09:37 -0500

    SprintChallenge - Authentication - Stretch - login, register, logout, display jokes completed
    Completed the full flow of register, login, display and logout. Tested and it works fine.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
 import LoginForm from "./auth/LoginForm";
 import RegisterForm from "./auth/RegisterForm";
 import JokesList from "./jokes/JokesList";
@@ -8,41 +8,42 @@ import JokesList from "./jokes/JokesList";
 import "../styles/App.css";
 
 class App extends React.Component {
-  state = {
-    isLoggedIn: false
-  };
-
-  handleLogout = e => {
-    e.preventDefault();
-    alert("Clicking logout");
-    // localStorage.removeItem("jwt");
-    // window.location.reload();
+  handleLogout = () => {
+    //alert("Clicking logout");
+    localStorage.removeItem("jwt");
+    this.setState({ isLoggedIn: false });
+    this.props.history.push("/login");
   };
 
   render() {
+    const isLoggedIn = localStorage.getItem("jwt") ? true : false;
+
     return (
-      <Router>
-        <div className="App">
-          <div className="App-card">
-            <div className="nav-bar">
+      <div className="App">
+        <div className="App-card">
+          <div className="nav-bar">
+            {!isLoggedIn && (
               <NavLink to="/login" className="nav-link">
                 Login
               </NavLink>
+            )}
+
+            {!isLoggedIn && (
               <NavLink to="/register" className="nav-link">
                 Register
               </NavLink>
-              <button onClick={this.handleLogout}>Logout</button>
-            </div>
-            <main>
-              <Route path="/login" component={LoginForm} />
-              <Route path="/register" component={RegisterForm} />
-              <Route path="/jokes" component={JokesList} />
-            </main>
+            )}
+            {isLoggedIn && <button onClick={this.handleLogout}>Logout</button>}
           </div>
+          <main>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/jokes" component={JokesList} />
+          </main>
         </div>
-      </Router>
+      </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
