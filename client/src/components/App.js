b8c861f1a3a9abf5e4bb1860c40f72a70f251027
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-14 10:47:32 -0500

    SprintChallenge - Authentication - Stretch - Client installed
    Created a react app using create-react-app. Removed unnecessary files and components and have the initial working version with server running

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hi There!!</header>
+    </div>
+  );
+}
+
+export default App;
