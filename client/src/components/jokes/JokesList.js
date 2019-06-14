1b9bfaf90101a94de319bfa88e9d91065f632e32
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-14 11:10:34 -0500

    SprintChallenge - Authentication - Stretch - Displaying jokes when logged in
    The authentication is set up for the jokes to be displayed when the user is logged in

--- client/src/components/jokes/JokesList.js
+++ client/src/components/jokes/JokesList.js
@@ -1,6 +1,41 @@
 import React from "react";
+import axios from "axios";
 
-function JokesList() {
-  return <div>In Jokes List!</div>;
+import "../auth/addInterceptors";
+import requireAuth from "../auth/requireAuth";
+
+class JokesList extends React.Component {
+  state = {
+    jokes: []
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Awesome Jokes!!</h2>
+
+        <ul>
+          {this.state.jokes.map(j => (
+            <li key={j.id}>{j.joke}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  componentDidMount() {
+    const endpoint = "/jokes";
+
+    axios
+      .get(endpoint)
+      .then(res => {
+        //console.log("jokes", JSON.stringify(res.data));
+        this.setState(() => ({ jokes: res.data }));
+      })
+      .catch(({ response }) => {
+        console.error("jokes error", response);
+      });
+  }
 }
-export default JokesList;
+
+export default requireAuth(JokesList);
